cbc957553fd49d3c3e366d729c8836abca2ea599
Orcastraw <jman22597@gmail.com>
2015-09-24 05:58:52 -0500

    first upload
    yep

---
+++ javascript.js
@@ -0,0 +1,126 @@
+$(document).ready(function() {
+    $('.xb1,.n64,.wiiu,.ps4,.ds,.3ds,.gba,.gbc,.gc,.wii,.psp,.pc').click(function() {
+     $('#rem').hide();
+    });
+});
+
+$(document).ready(function() {
+    $('.xb1,.n64,.wiiu,.ps4,.ds,.3ds,.gba,.gbc,.gc,.wii,.psp').click(function() {
+     $('#pc').hide();
+    });
+});
+$(document).ready(function() {
+    $('.pc').click(function() {
+     $('#pc').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.n64,.wiiu,.ps4,.ds,.3ds,.gba,.gbc,.gc,.wii,.psp').click(function() {
+     $('#xb1').hide();
+    });
+});
+$(document).ready(function() {
+    $('.xb1').click(function() {
+     $('#xb1').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.n64,.ps4,.ds,.3ds,.gba,.gbc,.gc,.wii,.psp').click(function() {
+     $('#wiiu').hide();
+    });
+});
+$(document).ready(function() {
+    $('.wiiu').click(function() {
+     $('#wiiu').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.wiiu,.n64,.ds,.3ds,.gba,.gbc,.gc,.wii,.psp').click(function() {
+     $('#ps4').hide();
+    });
+});
+$(document).ready(function() {
+    $('.ps4').click(function() {
+     $('#ps4').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.wiiu,.ps4,.n64,.3ds,.gba,.gbc,.gc,.wii,.psp').click(function() {
+     $('#ds').hide();
+    });
+});
+$(document).ready(function() {
+    $('.ds').click(function() {
+     $('#ds').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.wiiu,.ps4,.ds,.n64,.gba,.gbc,.gc,.wii,.psp').click(function() {
+     $('#tds').hide();
+    });
+});
+$(document).ready(function() {
+    $('.3ds').click(function() {
+     $('#tds').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.wiiu,.ps4,.ds,.3ds,.n64,.gbc,.gc,.wii,.psp').click(function() {
+     $('#gba').hide();
+    });
+});
+$(document).ready(function() {
+    $('.gba').click(function() {
+     $('#gba').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.wiiu,.ps4,.ds,.3ds,.gba,.n64,.gc,.wii,.psp').click(function() {
+     $('#gbc').hide();
+    });
+});
+$(document).ready(function() {
+    $('.gbc').click(function() {
+     $('#gbc').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.wiiu,.ps4,.ds,.3ds,.gba,.gbc,.n64,.wii,.psp').click(function() {
+     $('#gc').hide();
+    });
+});
+$(document).ready(function() {
+    $('.gc').click(function() {
+     $('#gc').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.wiiu,.ps4,.ds,.3ds,.gba,.gbc,.gc,.n64,.psp').click(function() {
+     $('#wii').hide();
+    });
+});
+$(document).ready(function() {
+    $('.wii').click(function() {
+     $('#wii').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.wiiu,.ps4,.ds,.3ds,.gba,.gbc,.gc,.wii,.n64').click(function() {
+     $('#psp').hide();
+    });
+});
+$(document).ready(function() {
+    $('.psp').click(function() {
+     $('#psp').show();
+    });
+});
+$(document).ready(function() {
+    $('.pc,.xb1,.wiiu,.ps4,.ds,.3ds,.gba,.gbc,.gc,.wii,.psp').click(function() {
+     $('#n64').hide();
+    });
+});
+$(document).ready(function() {
+    $('.n64').click(function() {
+     $('#n64').show();
+    });
+});
